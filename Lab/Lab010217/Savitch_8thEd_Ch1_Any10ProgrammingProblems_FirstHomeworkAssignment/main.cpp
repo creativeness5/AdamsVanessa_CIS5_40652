b0bb32aedf49533fdf1eda9ce7abb5e4f6d80140
Unknown <darkangelalways5@aol.com>
2018-01-14 12:53:58 -0800

    Testing
    Testing

---
+++ Lab/Lab010217/Savitch_8thEd_Ch1_Any10ProgrammingProblems_FirstHomeworkAssignment/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Vanessa Adams
+ * Created on January 6, 2018, 12:17 PM
+ * Purpose:  Any 10 Programming Problems - First Homework Assignment
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    
+    //Exit stage right!
+    return 0;
+}
